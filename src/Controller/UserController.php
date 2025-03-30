4a57b8799b038a5ceed6c66923ece3ea199f3848
Silvertiti <metheotim2@gmail.com>
2025-03-30 15:34:44 +0200

    Modification du register pour ajouter la séléction par rôle

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,6 +8,7 @@ use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
+use App\Middlewares\RoleCheckMiddleware;
 
 class UserController
 {
@@ -18,32 +19,61 @@ class UserController
         $this->container = $container;
     }
 
+
     public function registerRoutes($app)
     {
-        $app->get('/register', [self::class, 'createForm']);
-        $app->post('/register', [self::class, 'store']);
+        $app->get('/register', [UserController::class, 'createForm'])->add(RoleCheckMiddleware::class);
+        $app->post('/register', [UserController::class, 'store'])->add(RoleCheckMiddleware::class);
+        
     }
-
     public function createForm(Request $request, Response $response): Response
     {
         $view = Twig::fromRequest($request);
+        $session = $this->container->get('session');
+        $role = $session->get('role');
+    
+        $availableRoles = [];
+    
+        if ($role === 'admin') {
+            $availableRoles = ['user', 'tuteur'];
+        } elseif ($role === 'tuteur') {
+            $availableRoles = ['user'];
+        }
+    
         return $view->render($response, 'create_user.twig', [
-            'title' => 'Créer un utilisateur'
+            'title' => 'Créer un utilisateur',
+            'availableRoles' => $availableRoles
         ]);
     }
+    
+    
 
     public function store(Request $request, Response $response): Response
     {
         $data = $request->getParsedBody();
         $em = $this->container->get(EntityManager::class);
-
+        $session = $this->container->get('session');
+        $creatorRole = $session->get('role');
+    
+        $availableRoles = match ($creatorRole) {
+            'admin' => ['user', 'tuteur'],
+            'tuteur' => ['user'],
+            default => [],
+        };
+    
+        if (!in_array($data['role'], $availableRoles)) {
+            $response->getBody()->write("Rôle non autorisé.");
+            return $response->withStatus(403);
+        }
+    
         $user = new User($data['email'], $data['name']);
         $user->setMotDePasse(password_hash($data['password'], PASSWORD_BCRYPT));
         $user->setRole($data['role']);
-
+    
         $em->persist($user);
         $em->flush();
-
+    
         return $response->withHeader('Location', '/parametres')->withStatus(302);
     }
+    
 }
