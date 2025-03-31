8a46c84e2e58d629aed7918445815691668cca95
Mathéo Remy <matheo.remy@viacesi.fr>
2025-03-31 12:04:31 +0200

    Back Up Midi 31/03/25

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -24,7 +24,8 @@ class UserController
     {
         $app->get('/register', [UserController::class, 'createForm'])->add(RoleCheckMiddleware::class);
         $app->post('/register', [UserController::class, 'store'])->add(RoleCheckMiddleware::class);
-        
+        $app->get('/parametres/mon-compte', [UserController::class, 'editForm'])->add(RoleCheckMiddleware::class);
+        $app->post('/parametres', [UserController::class, 'update'])->add(RoleCheckMiddleware::class);
     }
     public function createForm(Request $request, Response $response): Response
     {
@@ -78,5 +79,53 @@ class UserController
     
         return $response->withHeader('Location', '/parametres')->withStatus(302);
     }
+
+    public function editForm(Request $request, Response $response): Response
+    {
+        $view = Twig::fromRequest($request);
+        $session = $this->container->get('session');
+        $userId = $session->get('id');  // L'ID de l'utilisateur connecté
+        $em = $this->container->get(EntityManager::class);
+        $user = $em->getRepository(User::class)->find($userId);
+
+        if (!$user) {
+            $response->getBody()->write("Utilisateur non trouvé.");
+            return $response->withStatus(404);
+        }
+
+        return $view->render($response, 'edit_user.twig', [
+            'title' => 'Modifier mon compte',
+            'user' => $user
+        ]);
+    }
     
+
+    public function update(Request $request, Response $response): Response
+    {
+        $data = $request->getParsedBody();
+        $session = $this->container->get('session');
+        $userId = $session->get('id');  // ID de l'utilisateur connecté
+        $em = $this->container->get(EntityManager::class);
+        $user = $em->getRepository(User::class)->find($userId);
+
+        if (!$user) {
+            $response->getBody()->write("Utilisateur non trouvé.");
+            return $response->withStatus(404);
+        }
+
+        // Mise à jour des informations
+        $user->setPrenom($data['prenom']);
+        $user->setNom($data['nom']);
+        $user->setEmail($data['email']);
+
+        // Si un mot de passe est fourni, le mettre à jour
+        if (!empty($data['password'])) {
+            $user->setPassword(password_hash($data['password'], PASSWORD_BCRYPT));  // Assurez-vous de hasher le mot de passe
+        }
+
+        $em->flush();
+
+        return $response->withHeader('Location', '/parametres')->withStatus(302);
+    }
+
 }
