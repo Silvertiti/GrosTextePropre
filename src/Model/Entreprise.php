f41fa00f24b26fce78c0c23b1e83977c098f2d80
Silvertiti <metheotim2@gmail.com>
2025-04-02 00:55:36 +0200

    Niquel Entreprise

--- src/Model/Entreprise.php
+++ src/Model/Entreprise.php
@@ -1,53 +1,62 @@
-<?php   
+<?php
+
 namespace App\Model;
 
+use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
-#[ORM\Table(name: "entreprises")]
-class Entreprise  // Retirer "final"
+#[ORM\Table(name: 'entreprises')]
+final class Entreprise
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column(type: "integer")]
+    #[ORM\Id, ORM\Column(type: 'integer'), ORM\GeneratedValue(strategy: 'AUTO')]
     private int $id;
 
-    #[ORM\Column(type: "string")]
-    private string $nom;
+    #[ORM\Column(type: 'string', nullable: false)]
+    private string $SIRET;
 
-    #[ORM\Column(type: "string")]
+    #[ORM\Column(type: 'string', unique: true, nullable: false)]
     private string $email;
 
-    #[ORM\Column(type: "string", nullable: true)]
-    private ?string $numeroTelephone = null;
+    #[ORM\Column(type: 'string')]
+    private string $numeroTelephone;
 
-    #[ORM\Column(type: "string", nullable: true)]
-    private ?string $lienSiteWeb = null;
+    #[ORM\Column(type: 'string', nullable: false)]
+    private string $nom;
 
-    #[ORM\Column(type: "text", nullable: true)]
-    private ?string $noteEvaluation = null;
+    #[ORM\Column(type: 'integer')]
+    private string $noteEvaluation;
 
-    #[ORM\Column(type: "datetimetz_immutable", nullable: false)]
-    private \DateTimeImmutable $registeredAt;
+    #[ORM\Column(type: 'integer')]
+    private string $lienSiteWeb;
 
-    #[ORM\Column(type: "string", nullable: true)]
-    private ?string $description;
+    #[ORM\Column(type: 'string')]
+    private ?string $description = null;
 
-    public function __construct(string $SIRET, string $email, string $numeroTelephone, string $nom, string $noteEvaluation, string $lienSiteWeb, ?string $description)
+
+    #[ORM\Column(name: 'registered_at', type: 'datetimetz_immutable', nullable: false)]
+
+    private DateTimeImmutable $registeredAt;
+
+    #[ORM\Column(type: 'integer')]
+    private int $vues = 0;
+
+
+    public function __construct(string $SIRET, string $email, string $numeroTelephone, string $nom, string $noteEvaluation, string $lienSiteWeb, string $description)
     {
         $this->SIRET = $SIRET;
         $this->email = $email;
         $this->numeroTelephone = $numeroTelephone;
         $this->nom = $nom;
         $this->noteEvaluation = $noteEvaluation;
+        $this->registeredAt = new DateTimeImmutable('now');
         $this->lienSiteWeb = $lienSiteWeb;
         $this->description = $description;
-        $this->registeredAt = new \DateTimeImmutable('now');
     }
     
 
     public function getId(): int { return $this->id; }
-    public function getSIRET(): string { return $this->SIRET ?? ''; }
+    public function getSIRET(): string { return $this->SIRET; }
     public function getEmail(): string { return $this->email; }
     public function getNumeroTelephone(): string { return $this->numeroTelephone; }
     public function getRegisteredAt(): DateTimeImmutable { return $this->registeredAt; }
@@ -63,4 +72,15 @@ class Entreprise  // Retirer "final"
     public function setNoteEvaluation(string $noteEvaluation): void { $this->noteEvaluation = $noteEvaluation; }
     public function setLienSiteWeb(string $lienSiteWeb): void { $this->lienSiteWeb = $lienSiteWeb; }
     public function setDescription(string $description): void { $this->description = $description; }
+
+    public function getVues(): int
+{
+    return $this->vues;
+}
+
+public function setVues(int $vues): void
+{
+    $this->vues = $vues;
+}
+
 }
